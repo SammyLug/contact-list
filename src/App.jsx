583e058a13a4ff071c5326acd2175ea386d4a726
Samantha Lugtu <sammylugtu@samanthas-air.mynetworksettings.com>
2024-02-19 19:51:22 -0500

    need to complete render single contact

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
 import './App.css'
-import ContactList from './components/ContactList'
+import { useState } from 'react';
+import ContactList from './components/ContactList';
+import SelectedContact from './components/SelectedContact';
 
 function App() {
+  const [SelectedContactId, setSelectedContactId] = useState(null);
   return (
     <>
-      <ContactList />
+    {SelectedContactId ? ( 
+      <div>Selected Contact View</div>
+    ) : (
+      <ContactList setSelectedContactId={}/>
+      )}
     </>
-  )
+  );
 }
 
 export default App
